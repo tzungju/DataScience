c8227656b119cd423cbc1a75c34f53e1c36cc396
mc6666 <mc6666@gmail.com>
2018-07-11 16:10:57 +0800

    v1

---
+++ Day1/plot1.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+a = [1, 2, 3, 4]
+b = [3, 9, 2, 6]
+plt.plot(a, b)
+plt.show()
---
+++ Day1/scatter.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+a = [1, 2, 3, 4]
+b = [3, 9, 2, 6]
+plt.scatter(a, b)
+plt.show()
